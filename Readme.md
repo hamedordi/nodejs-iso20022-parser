0cb4b2ef3cad00217564659b0df649c2c2ed111e
Debian <admin@ip-172-26-14-112.eu-north-1.compute.internal>
2022-06-16 13:06:16 +0000

    inital commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# XML for to Json file convertor
+
+## using fast-xml-parser
+- to convert an xml file to json file use this command
+node parser-with-fast-xml-parser.js -f ./sample-iso-files/seb/camt/camt.052_StartOfDay_Without_details.xml
---
+++ parser-with-fast-xml-parser.js
@@ -0,0 +1,60 @@
+// https://github.com/NaturalIntelligence/fast-xml-parser/blob/HEAD/docs/v4/2.XMLparseOptions.md
+
+const { XMLParser } = require('fast-xml-parser');
+const fs = require('fs');
+
+var argv = require('yargs/yargs')(process.argv.slice(2))
+    .usage('Usage: node $0 -f [filepath] ')
+    .example('node $0 -f /home/foo.xml', 'converts the /home/foo.xml to json and output is written in file ./parser-output.json')
+    .nargs('f', 1)
+    .alias('f', 'file')
+    .describe('f', 'parses an xml file and converts to json')
+    .demandOption(['f'])
+    .strict()
+    .argv;
+
+if (!argv.f || argv.f === "")
+    console
+
+// const alwaysArray = [
+//     "root.a.c",
+//     "root.b"
+// ];
+
+const options = {
+    ignoreAttributes: false,
+    attributeNamePrefix: "@_",
+    attributesGroupName: "attributes@_",
+    allowBooleanAttributes: true,
+    alwaysCreateTextNode: false,
+    cdataPropName: "__cdata",
+    ignoreDeclaration: false,
+    ignorePiTags: false,
+    textNodeName: "$text"
+    // isArray: (name, jpath, isLeafNode, isAttribute) => {  //force tags which are array
+    //     if( alwaysArray.indexOf(jpath) !== -1) return true;
+    // }
+    // stopNodes: ["root.a"] //stop parser from parsing tags nested tags
+    //unpairedTags: ["unpaired"] //specifying which tags should be treated as unpaired i.e. <br >
+
+
+};
+
+const parser = new XMLParser(options);
+
+fs.readFile(argv.f, function (err, data) {
+    (err) 
+    if (err) {
+        return console.log(err);
+    }
+    let jsonObj = parser.parse(data);
+    console.log("to json ->", JSON.stringify(jsonObj, null, '\t'));
+    fs.writeFile('./parser-output.json', JSON.stringify(jsonObj, null, '\t'), function (err, d) {
+        if (err) {
+            return console.log(err);
+        }
+    });
+});
+
+
+
